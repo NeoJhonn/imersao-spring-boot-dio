5f41947375c78bf3c1b9de6b631411d1daec0f34
NeoJhonn <jhonny.azevedo@gmail.com>
2024-06-14 00:16:20 -0300

    Adicionado MVP exemplo Spring Data JPA

---
+++ aulas/dio_exemplo_spring_data_jpa/src/main/java/dio/jhonny_azevedo/dio_exemplo_spring_data_jpa/DioExemploSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package dio.jhonny_azevedo.dio_exemplo_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioExemploSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioExemploSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ aulas/dio_exemplo_spring_data_jpa/src/test/java/dio/jhonny_azevedo/dio_exemplo_spring_data_jpa/DioExemploSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.jhonny_azevedo.dio_exemplo_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioExemploSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
